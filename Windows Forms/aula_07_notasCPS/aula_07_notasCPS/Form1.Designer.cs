db8cb37535e9761313de6ad8b24e79b9e415b485
VTheodoro <victorgft@outlook.com>
2024-03-13 11:09:38 -0300

    Successfull Update!

---
+++ Windows Forms/aula_07_notasCPS/aula_07_notasCPS/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace aula_07_notasCPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nota1 = new System.Windows.Forms.TextBox();
+            this.txt_nota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nota4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_nota3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_res = new System.Windows.Forms.Button();
+            this.txt_res = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_res);
+            this.groupBox1.Controls.Add(this.txt_nota4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_nota3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_nota2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_nota1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 162);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insira as  notas do aluno:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota 01:";
+            // 
+            // txt_nota1
+            // 
+            this.txt_nota1.Location = new System.Drawing.Point(5, 45);
+            this.txt_nota1.Name = "txt_nota1";
+            this.txt_nota1.Size = new System.Drawing.Size(100, 22);
+            this.txt_nota1.TabIndex = 1;
+            // 
+            // txt_nota2
+            // 
+            this.txt_nota2.Location = new System.Drawing.Point(121, 45);
+            this.txt_nota2.Name = "txt_nota2";
+            this.txt_nota2.Size = new System.Drawing.Size(100, 22);
+            this.txt_nota2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota 02:";
+            // 
+            // txt_nota4
+            // 
+            this.txt_nota4.Location = new System.Drawing.Point(121, 87);
+            this.txt_nota4.Name = "txt_nota4";
+            this.txt_nota4.Size = new System.Drawing.Size(100, 22);
+            this.txt_nota4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota 04:";
+            // 
+            // txt_nota3
+            // 
+            this.txt_nota3.Location = new System.Drawing.Point(5, 87);
+            this.txt_nota3.Name = "txt_nota3";
+            this.txt_nota3.Size = new System.Drawing.Size(100, 22);
+            this.txt_nota3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nota 03:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(45, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Menção Final do Aluno:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btn_res
+            // 
+            this.btn_res.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_res.Location = new System.Drawing.Point(72, 126);
+            this.btn_res.Name = "btn_res";
+            this.btn_res.Size = new System.Drawing.Size(75, 23);
+            this.btn_res.TabIndex = 8;
+            this.btn_res.Text = "Confirmar";
+            this.btn_res.UseVisualStyleBackColor = false;
+            this.btn_res.Click += new System.EventHandler(this.btn_res_Click);
+            // 
+            // txt_res
+            // 
+            this.txt_res.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txt_res.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_res.Location = new System.Drawing.Point(74, 214);
+            this.txt_res.Name = "txt_res";
+            this.txt_res.ReadOnly = true;
+            this.txt_res.Size = new System.Drawing.Size(100, 26);
+            this.txt_res.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 252);
+            this.Controls.Add(this.txt_res);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Média de Notas CPS";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_res;
+        private System.Windows.Forms.TextBox txt_nota4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_nota3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_nota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nota1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_res;
+    }
+}
+
---
+++ Windows Forms/aula_07_notasCPS/aula_07_notasCPS/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula_07_notasCPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_res_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, nota4;
+
+            nota1 = double.Parse(txt_nota1.Text);
+            nota2 = double.Parse(txt_nota2.Text);
+            nota3 = double.Parse(txt_nota3.Text);
+            nota4 = double.Parse(txt_nota4.Text);
+
+            double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (media <= 5)
+            {
+                txt_res.Text = "I";
+                txt_res.ForeColor = Color.Red;
+            }
+            else if (media > 5 && media <= 6)
+            {
+                txt_res.Text = "R";
+                txt_res.ForeColor = Color.Orange;
+            }
+            else if(media >= 7 && media < 9)
+            {
+                txt_res.Text = "B";
+                txt_res.ForeColor = Color.Blue;
+            }
+            else if (media >= 9)
+            {
+                txt_res.Text = "MB";
+                txt_res.ForeColor = Color.Blue;
+            }
+
+
+        }
+    }
+}
