5ed15a73998815a8205dc387e5d125c26eaaa9c6
2º ETIM - Desenvolvimento de Sistemas <victorgft@outlook.com>
2024-04-10 13:43:07 -0300

    Update!

--- 02 Programação Orientada a Objetos/04_exemplo_encapsulamento/04_exemplo_encapsulamento/Form1.Designer.cs
+++ 02 Programação Orientada a Objetos/04_exemplo_encapsulamento/04_exemplo_encapsulamento/Form1.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_cpf = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn_armazenar = new System.Windows.Forms.Button();
+            this.btn_get = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.lbl_cpf = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,19 +49,19 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Digite o Nome:";
             // 
-            // textBox1
+            // txt_nome
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txt_nome.Location = new System.Drawing.Point(32, 49);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 1;
             // 
-            // textBox2
+            // txt_cpf
             // 
-            this.textBox2.Location = new System.Drawing.Point(162, 49);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.txt_cpf.Location = new System.Drawing.Point(162, 49);
+            this.txt_cpf.Name = "txt_cpf";
+            this.txt_cpf.Size = new System.Drawing.Size(100, 20);
+            this.txt_cpf.TabIndex = 3;
             // 
             // label2
             // 
@@ -72,23 +72,25 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Digite o CPF:";
             // 
-            // button1
+            // btn_armazenar
             // 
-            this.button1.Location = new System.Drawing.Point(97, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Armazenar em Set";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_armazenar.Location = new System.Drawing.Point(97, 84);
+            this.btn_armazenar.Name = "btn_armazenar";
+            this.btn_armazenar.Size = new System.Drawing.Size(103, 23);
+            this.btn_armazenar.TabIndex = 4;
+            this.btn_armazenar.Text = "Armazenar em Set";
+            this.btn_armazenar.UseVisualStyleBackColor = true;
+            this.btn_armazenar.Click += new System.EventHandler(this.btn_armazenar_Click);
             // 
-            // button2
+            // btn_get
             // 
-            this.button2.Location = new System.Drawing.Point(97, 126);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Mostrar em Get";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_get.Location = new System.Drawing.Point(97, 126);
+            this.btn_get.Name = "btn_get";
+            this.btn_get.Size = new System.Drawing.Size(103, 23);
+            this.btn_get.TabIndex = 5;
+            this.btn_get.Text = "Mostrar em Get";
+            this.btn_get.UseVisualStyleBackColor = true;
+            this.btn_get.Click += new System.EventHandler(this.btn_get_Click);
             // 
             // label3
             // 
@@ -108,38 +110,38 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "CPF do Aluno:";
             // 
-            // label5
+            // lbl_nome
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(138, 165);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Mostrar Nome";
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(138, 165);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(73, 13);
+            this.lbl_nome.TabIndex = 8;
+            this.lbl_nome.Text = "Mostrar Nome";
             // 
-            // label6
+            // lbl_cpf
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(138, 190);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 13);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "Mostrar CPF";
+            this.lbl_cpf.AutoSize = true;
+            this.lbl_cpf.Location = new System.Drawing.Point(138, 190);
+            this.lbl_cpf.Name = "lbl_cpf";
+            this.lbl_cpf.Size = new System.Drawing.Size(65, 13);
+            this.lbl_cpf.TabIndex = 9;
+            this.lbl_cpf.Text = "Mostrar CPF";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(287, 242);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_cpf);
+            this.Controls.Add(this.lbl_nome);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_get);
+            this.Controls.Add(this.btn_armazenar);
+            this.Controls.Add(this.txt_cpf);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_nome);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -152,15 +154,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_cpf;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_armazenar;
+        private System.Windows.Forms.Button btn_get;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Label lbl_cpf;
     }
 }
 
--- 02 Programação Orientada a Objetos/04_exemplo_encapsulamento/04_exemplo_encapsulamento/Form1.cs
+++ 02 Programação Orientada a Objetos/04_exemplo_encapsulamento/04_exemplo_encapsulamento/Form1.cs
@@ -17,9 +17,20 @@ namespace _04_exemplo_encapsulamento
             InitializeComponent();
         }
 
+
         private void Form1_Load(object sender, EventArgs e)
         {
+        }
 
+        aluno z = new aluno();
+        private void btn_armazenar_Click(object sender, EventArgs e)
+        {
+            z.setNome(txt_nome.Text);
+        }
+
+        private void btn_get_Click(object sender, EventArgs e)
+        {
+            lbl_nome.Text = z.getNome();
         }
     }
 }
