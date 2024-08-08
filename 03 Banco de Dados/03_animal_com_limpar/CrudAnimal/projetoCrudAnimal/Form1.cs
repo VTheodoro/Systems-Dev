2a8af8c5168d04d73a4c8a813566a16f2e8141eb
2º ETIM - Desenvolvimento de Sistemas <victorgft@outlook.com>
2024-08-08 15:29:56 -0300

    Update!

---
+++ 03 Banco de Dados/03_animal_com_limpar/CrudAnimal/projetoCrudAnimal/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace projetoCrudAnimal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Animal a = new Animal();
+
+        private void btn_salvar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = a.Consultar();
+            txt_nome.BackColor = SystemColors.Window;
+            txt_idade.BackColor = SystemColors.Window;
+            txt_especie.BackColor = SystemColors.Window;
+            txt_peso.BackColor = SystemColors.Window;
+            txt_tamanho.BackColor = SystemColors.Window;
+
+            bool todosCamposPreenchidos = true;
+
+            if (string.IsNullOrWhiteSpace(txt_nome.Text))
+            {
+                txt_nome.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_idade.Text))
+            {
+                txt_idade.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (!rdb_femea.Checked && !rdb_macho.Checked)
+            {
+                MessageBox.Show("Por favor, selecione o sexo.");
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_especie.Text))
+            {
+                txt_especie.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_peso.Text))
+            {
+                txt_peso.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_tamanho.Text))
+            {
+                txt_tamanho.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+
+            if (todosCamposPreenchidos)
+            {
+                try
+                {
+                    a.SetNome(txt_nome.Text);
+
+                    // Converta os campos para inteiros antes de definir
+                    a.SetIdade(int.Parse(txt_idade.Text));
+                    a.SetPeso(int.Parse(txt_peso.Text));
+                    a.SetTamanho(int.Parse(txt_tamanho.Text));
+                    a.SetEspecie(txt_especie.Text);
+
+                    if (rdb_femea.Checked)
+                    {
+                        a.SetSexo("Fêmea");
+                    }
+                    else if (rdb_macho.Checked)
+                    {
+                        a.SetSexo("Macho");
+                    }
+
+                    a.Inserir();
+                    MessageBox.Show("Informações salvas com sucesso!");
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("Erro de formatação: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao salvar as informações: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, preencha todos os campos obrigatórios.");
+            }
+        }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = a.Consultar();
+            dataGridView1.Columns["nome_animal"].HeaderText = "Nome";
+            dataGridView1.Columns["idade_animal"].HeaderText = "Idade";
+            dataGridView1.Columns["sexo_animal"].HeaderText = "Sexo";
+            dataGridView1.Columns["especie_animal"].HeaderText = "Especie";
+            dataGridView1.Columns["peso_animal"].HeaderText = "Peso";
+            dataGridView1.Columns["tamanho_animal"].HeaderText = "Tamanho";
+
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = a.Consultar();
+            if (string.IsNullOrWhiteSpace(txt_nome.Text))
+            {
+                MessageBox.Show("Por favor, informe o nome do animal para exclusão.");
+                return;
+            }
+
+            try
+            {
+                a.SetNome(txt_nome.Text);
+                a.Excluir();
+                dataGridView1.DataSource = a.Consultar(); // Atualiza a lista após exclusão
+                MessageBox.Show("As informações selecionadas foram deletadas com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao excluir as informações: " + ex.Message);
+            }
+        }
+    
+
+        private void txt_nome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_sexo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_tamanho_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_idade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_peso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_alterar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = a.Consultar();
+            // Verificar se todos os campos obrigatórios estão preenchidos
+            txt_nome.BackColor = SystemColors.Window;
+            txt_idade.BackColor = SystemColors.Window;
+            txt_especie.BackColor = SystemColors.Window;
+            txt_peso.BackColor = SystemColors.Window;
+            txt_tamanho.BackColor = SystemColors.Window;
+
+            bool todosCamposPreenchidos = true;
+
+            if (string.IsNullOrWhiteSpace(txt_nome.Text))
+            {
+                txt_nome.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_idade.Text))
+            {
+                txt_idade.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (!rdb_femea.Checked && !rdb_macho.Checked)
+            {
+                MessageBox.Show("Por favor, selecione o sexo.");
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_especie.Text))
+            {
+                txt_especie.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_peso.Text))
+            {
+                txt_peso.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+            if (string.IsNullOrWhiteSpace(txt_tamanho.Text))
+            {
+                txt_tamanho.BackColor = Color.Red;
+                todosCamposPreenchidos = false;
+            }
+
+            if (todosCamposPreenchidos)
+            {
+                try
+                {
+                    a.SetNome(txt_nome.Text);
+
+                    a.SetIdade(int.Parse(txt_idade.Text));
+                    a.SetPeso(int.Parse(txt_peso.Text));
+                    a.SetTamanho(int.Parse(txt_tamanho.Text));
+                    a.SetEspecie(txt_especie.Text);
+
+                    if (rdb_femea.Checked)
+                    {
+                        a.SetSexo("Fêmea");
+                    }
+                    else if (rdb_macho.Checked)
+                    {
+                        a.SetSexo("Macho");
+                    }
+
+                    a.Alterar(); 
+
+                    dataGridView1.DataSource = a.Consultar();
+
+                    MessageBox.Show("Informações atualizadas com sucesso!");
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("Erro de formatação: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao atualizar as informações: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, preencha todos os campos obrigatórios.");
+            }
+
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = a.Consultar();
+            txt_nome.Text = "";
+            txt_especie.Text = "";
+            txt_peso.Text = "";
+            txt_idade.Text = "";
+            txt_tamanho.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            exibiregistro(dataGridView1.CurrentRow.Index);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void exibiregistro(int i)
+        {
+            txt_nome.Text = "" + dataGridView1[0,i].Value;
+            txt_especie.Text = "" + dataGridView1[3,i].Value;
+            txt_peso.Text = "" + dataGridView1[4,i].Value;
+            txt_idade.Text = "" + dataGridView1[1,i].Value;
+            txt_tamanho.Text = "" + dataGridView1[5,i].Value;
+        }
+    }
+}
---
+++ 03 Banco de Dados/03_animal_com_limpar/CrudAnimal/projetoCrudAnimal/animal.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace projetoCrudAnimal
+{
+    class Animal : conexao
+    {
+        private string nome;
+        private int idade;
+        private string sexo;
+        private string especie;
+        private int peso;
+        private int tamanho;
+
+        public void SetNome(string nome) => this.nome = nome;
+        public string GetNome() => this.nome;
+
+        public void SetIdade(int idade) => this.idade = idade;
+        public int GetIdade() => this.idade;
+
+        public void SetSexo(string sexo) => this.sexo = sexo;
+        public string GetSexo() => this.sexo;
+
+        public void SetEspecie(string especie) => this.especie = especie;
+        public string GetEspecie() => this.especie;
+
+        public void SetPeso(int peso) => this.peso = peso;
+        public int GetPeso() => this.peso;
+
+        public void SetTamanho(int tamanho) => this.tamanho = tamanho;
+        public int GetTamanho() => this.tamanho;
+
+        public void Inserir()
+        {
+            string query = "INSERT INTO animal (nome_animal, idade_animal, sexo_animal, especie_animal, peso_animal, tamanho_animal) " +
+                           "VALUES (@nome, @idade, @sexo, @especie, @peso, @tamanho)";
+
+            if (this.abrirconexao())
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conectar))
+                {
+                    cmd.Parameters.AddWithValue("@nome", GetNome());
+                    cmd.Parameters.AddWithValue("@idade", GetIdade());
+                    cmd.Parameters.AddWithValue("@sexo", GetSexo());
+                    cmd.Parameters.AddWithValue("@especie", GetEspecie());
+                    cmd.Parameters.AddWithValue("@peso", GetPeso());
+                    cmd.Parameters.AddWithValue("@tamanho", GetTamanho());
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (MySqlException ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Erro ao inserir dados: " + ex.Message);
+                    }
+                }
+                this.fecharconexao();
+            }
+        }
+
+        public void Excluir()
+        {
+            string query = "DELETE FROM animal WHERE nome_animal = @nome";
+
+            if (this.abrirconexao())
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conectar))
+                {
+                    cmd.Parameters.AddWithValue("@nome", GetNome());
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (MySqlException ex)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Erro ao excluir dados: " + ex.Message);
+                    }
+                }
+                this.fecharconexao();
+            }
+        }
+
+        public void Alterar()
+        {
+            string query = "UPDATE animal SET idade_animal = @idade, sexo_animal = @sexo, especie_animal = @especie, peso_animal = @peso, tamanho_animal = @tamanho WHERE nome_animal = @nome";
+
+            if (this.abrirconexao())
+            {
+                try
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, conectar))
+                    {
+
+                        cmd.Parameters.AddWithValue("@nome", GetNome());
+                        cmd.Parameters.AddWithValue("@idade", GetIdade());
+                        cmd.Parameters.AddWithValue("@sexo", GetSexo());
+                        cmd.Parameters.AddWithValue("@especie", GetEspecie());
+                        cmd.Parameters.AddWithValue("@peso", GetPeso());
+                        cmd.Parameters.AddWithValue("@tamanho", GetTamanho());
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected == 0)
+                        {
+                            System.Windows.Forms.MessageBox.Show("Nenhum registro encontrado para atualizar.");
+                        }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    System.Windows.Forms.MessageBox.Show("Erro ao atualizar dados: " + ex.Message);
+                }
+                finally
+                {
+                    this.fecharconexao();
+                }
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Não foi possível abrir a conexão com o banco de dados.");
+            }
+        }
+
+       
+        public DataTable Consultar()
+        {
+            DataTable dt = new DataTable();
+            string mSQL = "SELECT * FROM animal";
+
+            if (this.abrirconexao())
+            {
+                using (MySqlCommand cmd = new MySqlCommand(mSQL, conectar))
+                {
+                    using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                    {
+                        try
+                        {
+                            da.Fill(dt);
+                        }
+                        catch (MySqlException ex)
+                        {
+                            System.Windows.Forms.MessageBox.Show("Erro ao consultar dados: " + ex.Message);
+                        }
+                    }
+                }
+                this.fecharconexao();
+            }
+
+            return dt;
+        }
+    }
+}
