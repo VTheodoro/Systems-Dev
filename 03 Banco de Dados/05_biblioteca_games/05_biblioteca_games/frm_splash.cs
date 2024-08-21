ee5a9a72ede17359f667bb0ff11f59272d273d1f
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-08-21 11:07:24 -0300

    Update

---
+++ 03 Banco de Dados/05_biblioteca_games/05_biblioteca_games/frm_splash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_biblioteca_games
+{
+    public partial class frm_splash : Form
+    {
+        public frm_splash()
+        {
+            InitializeComponent();
+        }
+    }
+}
